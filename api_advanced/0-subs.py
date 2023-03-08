e7afb7c29a4789bd334e7b24efcbfeaa19ebe523
kaybrian <kayonbrian@gmail.com>
2023-03-08 21:24:18 +0200

    docs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-0 - subs
-"""
-
+"""DOC"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Docs"""
+    """DOC"""
     reddit_url = f"https://www.reddit.com/r/{subreddit}/about.json"
     header = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url, headers=header)
