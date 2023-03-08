3c238977e5f733ebfe6d1fdc0994cf41acf1cda8
kaybrian <kayonbrian@gmail.com>
2023-03-08 22:12:54 +0200

    sent the new ones

---
+++ api_advanced/3-count.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""""Doc"""
+import requests
+
+
+def count_words(subreddit, word_list, after="", words_count={}):
+    """"Doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=100" \
+        .format(subreddit)
+    header = {'User-Agent': 'Mozilla/5.0'}
+    param = {'after': after}
+    res = requests.get(url, headers=header, params=param)
+
+    if res.status_code != 200:
+        return
+
+    json_res = res.json()  # chch
+    after = json_res.get('data').get('after')
+    has_next = after is not None
+    hot_titles = []
+    words = [word.lower() for word in word_list]
+
+    if len(words_count) == 0:
+        words_count = {word: 0 for word in words}
+    hot_articles = json_res.get('data').get('children')
+    [hot_titles.append(article.get('data').get('title'))
+     for article in hot_articles]
+
+    for i in range(len(hot_titles)):
+
+        for title_word in hot_titles[i].lower().split():
+            for word in words:
+                if word.lower() == title_word:
+                    words_count[word] = words_count.get(word) + 1
+
+    if has_next:
+        return count_words(subreddit, word_list, after, words_count)
+    else:
+
+        words_count = dict(filter(lambda item: item[1] != 0,
+                                  words_count.items()))
+        words_count = sorted(words_count.items(),
+                             key=lambda item: item[1],
+                             reverse=True)
+
+        for i in range(len(words_count)):
+            print("{}: {}".format(words_count[i][0],
+                                  words_count[i][1]))
